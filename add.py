8f21df871c7436b0c1c60df95a6f9bc3b855deba
VAIBHAV11464 <vaibhav11464@gmail.com>
2026-09-02 15:22:15 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 20
+    result = add_numbers(num1, num2)
+    print("=================================")
+    print("Addition Result")
+    print("=================================")
+    print(f"First Number : {num1}")
+    print(f"Second Number: {num2}")
+    print(f"Sum : {result}")
